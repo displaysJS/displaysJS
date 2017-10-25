2f510e3c8eb6226a273c0e57593f0354b7db0f9b
sxu2583 <shihabuddin100@gmail.com>
2017-10-25 17:37:36 -0400

    editing the youtubeDisp

---
+++ example/samples/youtube_display.js
@@ -0,0 +1,13 @@
+"use strict";
+import { Display } from "../../src/display.js";
+
+
+class YoutubeDisplay extends Display {
+
+  constructor(args) {
+    super(args);
+    this.text_canvas = document.createElement('div');
+  }
+
+
+}
